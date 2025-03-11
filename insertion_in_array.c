c2217e16b9b2441d0954ffd8de0c0431e96a7b75
Ronak Jat <ronakjat789@gmail.com>
2025-03-11 21:40:43 +0530

    Create insertion_in_array.c

---
+++ insertion_in_array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int insert(int arr[],int n,int size,int pos,int value)
+{
+    if (n>size){
+        printf("overflow condition");
+        return n;
+    }
+    for (int i = n ; i>pos; i--){
+        arr[i]=arr[i-1];;
+    }
+    arr[pos]=value;
+    return n+1;
+}
+
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
+
+int main() {
+int arr[] = {1, 2, 4, 5, 6}; 
+int size = 10;
+int n = sizeof(arr)/sizeof(arr[0]);
+int pos = 2, value = 3; 
+n = insert(arr, n, size, pos, value);
+printf("Updated Array: ");
+display(arr,n);
+return 0;
+}
